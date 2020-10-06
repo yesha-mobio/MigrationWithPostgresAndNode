6945432346d10e86c8f4bdfca363ddeddca99a68
yesha-mobio <yeshas.mobio@gmail.com>
2020-10-06 13:21:38 +0530

    Created tables and Associations.

--- migrations/20201005120237-create-tbl-user.js
+++ migrations/20201005120237-create-tbl-user.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('tbl_users', {
+    await queryInterface.createTable("tbl_users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       role_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('tbl_users');
-  }
+    await queryInterface.dropTable("tbl_users");
+  },
 };
--- models/tbl_bundle_product.js
+++ models/tbl_bundle_product.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class tbl_bundle_product extends Model {
     /**
@@ -10,17 +8,28 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      tbl_bundle_product.belongsTo(models.tbl_user, {
+        foreignKey: "created_by",
+        as: "bundleProductCreatedBy",
+      });
+      tbl_bundle_product.belongsTo(models.tbl_user, {
+        foreignKey: "updated_by",
+        as: "bundleProductUpdatedBy",
+      });
       // define association here
     }
-  };
-  tbl_bundle_product.init({
-    bundle_id: DataTypes.INTEGER,
-    product_id: DataTypes.INTEGER,
-    created_by: DataTypes.INTEGER,
-    updated_by: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'tbl_bundle_product',
-  });
+  }
+  tbl_bundle_product.init(
+    {
+      bundle_id: DataTypes.INTEGER,
+      product_id: DataTypes.INTEGER,
+      created_by: DataTypes.INTEGER,
+      updated_by: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "tbl_bundle_product",
+    }
+  );
   return tbl_bundle_product;
 };
